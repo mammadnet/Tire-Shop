79d33f9f5dac58b382f2fb379e668ec70d648e5d
Mohsen <moohamadiann@gmail.com>
2025-03-26 00:18:12 +0330

    Create new user in database first of all check if the national id of the new user is already exist in database, if not then create it

--- database/crud.py
+++ database/crud.py
@@ -1,2 +1,39 @@
+from models import User,Employee,Admin,Manager,Order,Customer,Product,Size,Brand
+
+from sqlalchemy.orm import Session, InstrumentedAttribute
+
+from sqlalchemy import select, exists
+
+from connection import session
+
+def exist_chek_user(by:InstrumentedAttribute, pat):
+    subq = exists(User.national_number).where(User.national_number == pat).select()
+    exist_check = session.execute(subq).scalar()
+    return exist_check
+    
+
+
+def create_new_user(session: Session, name:str, lastname:str, phone:str, national_number:str, level_type:str) -> User:
+    if level_type == 'admin':
+        new_user = Admin(name=name, lastname=lastname, phone=phone, national_number=national_number)
+    elif level_type == 'manager':
+        new_user = Manager(name=name, lastname=lastname, phone=phone, national_number=national_number)
+    elif level_type == 'employee':
+        new_user = Employee(name=name, lastname=lastname, phone=phone, national_number=national_number)
+    else:
+        return None
+    
+    
+    
+    with session as db:
+        exist_check = exist_chek_user(User.national_number, national_number)
+        
+        if not exist_check:
+            db.add(new_user)
+            db.commit()
+            
+    # TODO rais an error that the national code already exist
+    return None
+        
 
 
