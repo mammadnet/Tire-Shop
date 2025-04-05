79e1426098fe0ec76051b6b939915c237256b98a
Mohsen <moohamadiann@gmail.com>
2025-04-06 03:01:02 +0330

    Add default border color on entry and button wdget

--- interface/widgets.py
+++ interface/widgets.py
@@ -5,7 +5,7 @@ class Btn(CTkButton):
     def __init__(self, master,text, corner_radius, width, height, **kwargs):
         super().__init__(master, **kwargs)
         
-        self.configure(text=text, corner_radius = corner_radius, fg_color='#AFB3ED', hover_color='#888bba', text_color='#494A5F')
+        self.configure(text=text, corner_radius = corner_radius, fg_color='#AFB3ED', hover_color='#888bba', text_color='#494A5F', border_color='#8688B0')
         self.configure(width=width, height=height)
 
 
@@ -13,7 +13,7 @@ class Input(CTkEntry):
     def __init__(self,master, corner_radius, width, height, placeholder_text,**kwargs):
         super().__init__(master=master,corner_radius=corner_radius, width=width, height=height, placeholder_text=placeholder_text, **kwargs)
         
-        self.configure(fg_color='#646691', placeholder_text_color='#9495B8', text_color='#c5c6de')
+        self.configure(fg_color='#646691', placeholder_text_color='#9495B8', text_color='#c5c6de', border_color='#8688B0')
     
     def disable(self):
         self.configure(state='disabled')
