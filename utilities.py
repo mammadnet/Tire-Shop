aa2bff4e70cbc4a221b9843cdb14b9108f004c72
Mohsen <moohamadiann@gmail.com>
2025-04-09 17:47:02 +0330

    create a tread class for concurrent function call

--- utilities.py
+++ utilities.py
@@ -1,7 +1,15 @@
 from hashlib import sha256
-
+import threading
 
 def hashing(val:str) -> str:
     hashed = sha256(val.encode()).hexdigest()
     return hashed
     
+
+class Concur:
+    def __init__(self, func):
+        self.func = func
+        self.thread = threading.Thread(target=func)
+        
+    def start(self):
+        self.thread.start()
