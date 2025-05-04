57225db61bef387daac6eb3f5ae5275b57658fbd
Mohsen <moohamadiann@gmail.com>
2025-05-04 17:46:46 +0330

    Add functions for check os type (Linux or Windows)

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 from hashlib import sha256
 import threading
-
+from platform import system
 def hashing(val:str) -> str:
     hashed = sha256(val.encode()).hexdigest()
     return hashed
@@ -13,3 +13,12 @@ class Concur:
         
     def start(self):
         self.thread.start()
+        
+        
+def is_windows():
+    os_name = system()
+    return os_name == "Windows"
+
+def is_linux():
+    os_name = system()
+    return os_name == "Linux"
