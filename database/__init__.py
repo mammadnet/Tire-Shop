7f7944fc4ff8f12d01f276bec757af48a00d595b
Mohsen <moohamadiann@gmail.com>
2025-05-04 18:16:47 +0330

    Add database utilities function to database package

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,4 @@
 from .crud import login_permission, get_all_employees, get_all_employees_json
 from .connection import session
 from utilities import hashing
+from .utilities import is_admin, is_manager, is_employee
