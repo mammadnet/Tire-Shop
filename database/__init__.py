a3dd68031ee69e0f912c00385db71062ad6eff6d
Mohsen <moohamadiann@gmail.com>
2025-05-04 18:23:08 +0330

    add defined public function for database package

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,4 @@
-from .crud import login_permission, get_all_employees, get_all_employees_json
+from .crud import login_permission, get_all_employees, get_all_employees_json, user_by_username_pass, create_new_user
 from .connection import session
 from utilities import hashing
 from .utilities import is_admin, is_manager, is_employee
