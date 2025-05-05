b574f94145aaf0c9964dc5d8259a1c8169dadf8c
Mohsen <moohamadiann@gmail.com>
2025-05-05 11:25:54 +0330

    bind Admin page and Manager page with login page

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-from interface.ui import Login_page, Admin_page
+from interface.ui import Login_page, Admin_page, Manager_page, Page
 from interface.widgets import Root
 from customtkinter import *
-from database import login_permission, session
+from database import login_permission, session, is_admin,is_manager, user_by_username_pass
 root = Root()
-def login_action(username, password, login_page:Login_page):
-    res = login_permission(session, username, password)
-    if res:
-        login_page.destroy()
 
-        Admin_page(root)
+def login_action(username, password, login_page:Login_page):
+    user = user_by_username_pass(session, username, password)
+        
+    if is_admin(user):
+        Admin_page(root, user.name, user.lastname, user.type, logout_action)
+    elif is_manager(user):
+        Manager_page(root, user.name, user.lastname, user.type, logout_action)
+    
     else:
         # call a fucntion in side object
         login_page.login_error_message('Password or username is incorrect.')
+    if user:
+        login_page.destroy()
+
+def logout_action(active_page:Page):
+    active_page.destroy()
+    Login_page(root, login_action)
+    
         
-l = Login_page(root, login_action)
+Login_page(root, login_action)
+
 
 
 root.mainloop()
