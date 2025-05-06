3656dae455808a34b977f6b9900ce3466f1569ef
Mohsen <moohamadiann@gmail.com>
2025-05-06 16:30:53 +0330

    Add new create function to database package

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,4 @@
-from .crud import login_permission, get_all_employees, get_all_employees_json, user_by_username_pass, create_new_user
+from .crud import login_permission, get_all_employees, get_all_employees_json, user_by_username_pass, create_new_user, user_by_username,remove_user_by_username
 from .connection import session
 from utilities import hashing
 from .utilities import is_admin, is_manager, is_employee
