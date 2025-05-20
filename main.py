37e3e8337d2fbf73b263bfaa85ba1c9b178783f5
Shilan Fallahi <fallahishilan@gmail.com>
2025-05-20 18:56:52 +0330

    Added comments to main.py based on clean code principles

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
+# Import necessary modules and classes from other files
 from interface.ui import Login_page, Admin_page, Manager_page, Page
 from interface.widgets import Root
 from customtkinter import *
-from database import login_permission, session, is_admin,is_manager, user_by_username_pass
+from database import login_permission, session, is_admin, is_manager, user_by_username_pass
+
+# Initialize the root window for the application
 root = Root()
 
-def login_action(username, password, login_page:Login_page):
+# Define the login action function
+def login_action(username, password, login_page: Login_page):
+    # Retrieve user information based on username and password
     user = user_by_username_pass(session, username, password)
         
+    # Check if the user is an admin and navigate to the Admin page
     if is_admin(user):
         Admin_page(root, user.name, user.lastname, user.type, logout_action)
+    # Check if the user is a manager and navigate to the Manager page
     elif is_manager(user):
         Manager_page(root, user.name, user.lastname, user.type, logout_action)
-    
     else:
-        # call a fucntion in side object
         login_page.login_error_message('Password or username is incorrect.')
+    
     if user:
         login_page.destroy()
 
-def logout_action(active_page:Page):
+# Define the logout action function
+def logout_action(active_page: Page):
     active_page.destroy()
+    # Redirect to the login page
     Login_page(root, login_action)
-    
-        
+
 Login_page(root, login_action)
-
-
 
 root.mainloop()
