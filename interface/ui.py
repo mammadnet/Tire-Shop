d78d9cc435b810fda160f51cfbfd36d003eb366f
Mohsen <moohamadiann@gmail.com>
2025-05-23 14:09:51 +0330

    Create user edit panel on admin page for edit user information

--- interface/ui.py
+++ interface/ui.py
@@ -5,7 +5,7 @@ from .widgets import *
 from utilities import Concur, is_windows
 from time import sleep
 
-from database import get_all_employees,get_all_employees_json, session, create_new_user, remove_user_by_username
+from database import get_all_employees,get_all_employees_json, session, create_new_user, remove_user_by_username, update_user_by_username, get_all_username, user_by_username
 from database import UsernameAlreadyExistsException, NationalNumberAlreadyExistsException
 
 from PIL import Image
@@ -271,6 +271,9 @@ class Admin_page(Page):
             self.delete_user_comboBox = None
             self.delete_user_btn = None
             
+            self.edit_user_frame = None
+            self.edit_user_combobox = None
+            self.edit_user_inputs = None
             
             # Show table by default
             self.current_view = None
@@ -477,7 +480,7 @@ class Admin_page(Page):
             
             return True
                 
-                
+        #-----------------DELETE USER PANEL-------------------------
                 
         def delete_user(self, window, users:list):
             
@@ -521,11 +524,105 @@ class Admin_page(Page):
             show_success_msg_callback("User was deleted")
             self.delete_user(self, get_all_employees_json(session))
         
+        #-----------------------------------------------------------------
+        
+        def employee_edit(self, window):
+            
+            if self.edit_user_frame:
+                content_frame = self.edit_user_frame
+            else:
+                content_frame = CTkFrame(window, fg_color="#5B5D76")
+                self.edit_user_frame = content_frame
+                
+            content_frame.place(relheight=.9, relwidth=.8, relx=.02, rely=.05)
+            content_frame.rowconfigure(tuple(range(0, 8)), weight=1)
+            content_frame.columnconfigure((0, 3), weight=1, pad=40, uniform='a')
+
+            # Label and dropdown to select user by username
+            select_user_label = CTkLabel(content_frame, text="Username:", text_color="white", font=(None, 15))
+            select_user_label.grid(row=0, column=0)
+
+            usernames = get_all_username(session)  # Fetch list of usernames
+            selected_username = StringVar()
+            if self.edit_user_combobox:
+                self.edit_user_combobox.grid_forget()
+                self.edit_user_combobox.destroy()
+            print("hereeeeeeeeeeeeeee")
+            self.edit_user_combobox = DropDown(content_frame, values=usernames, variable=selected_username, command=self.load_user_data)
+            self.edit_user_combobox.grid(row=0, column=1)
+
+            # Dictionary to hold Input objects
+            if not self.edit_user_inputs:
+                self.edit_user_inputs = {}
+            
+            self.create_input_field(content_frame, "Name:", 1, 0, 'name')
+            self.create_input_field(content_frame, "Lastname:", 1, 2, 'lastname')
+            self.create_input_field(content_frame, "National Number:", 2, 0, 'national')
+            self.create_input_field(content_frame, "Phone Number:", 2, 2, 'phone')
+            self.create_input_field(content_frame, "Username:", 3, 0, 'username')
+            
+            
+            
+            update_btn = Btn(content_frame, 160, 45)
+            update_btn.configure(command=lambda: self.update_user_action(
+                self.edit_user_combobox.get(),
+                self.edit_user_inputs['name'].get(),
+                self.edit_user_inputs['lastname'].get(),
+                self.edit_user_inputs['phone'].get(),
+                self.edit_user_inputs['national'].get(),
+                self.edit_user_inputs['username'].get(),
+                None,
+                self.show_error_message,
+                self.show_success_message
+                # self.edit_user_inputs['password'].get(),
+                # self.edit_user_inputs['password_repeat'].get(),
+            ))
+            update_btn.configure(font=(None, 16))
+            update_btn.set_text(text="بروزرسانی اطلاعات")
+            update_btn.grid(row=6, column=0, columnspan=4)
+
+        # Helper function to create an input field
+        def create_input_field(self, window, label_text, row, column, field_key, **kwargs):
+            if field_key not in self.edit_user_inputs:
+                label = CTkLabel(window, text=label_text, text_color="white", font=(None, 13))
+                label.grid(row=row, column=column, **kwargs)
+                var = StringVar()
+                input_widget = Input(window, 15, 150, 35, None, var, placeholder_empty=False)
+                input_widget.set_textvariable(var)
+                input_widget.textvariable.set('')
+                input_widget.grid(row=row, column=column+1)
+                self.edit_user_inputs[field_key] = input_widget
+
+
+        # Load selected user's data
+        def load_user_data(self, username):
+            user_data = user_by_username(session, username)
+            if user_data:
+                self.edit_user_inputs['name'].set_placeholder_text(user_data.name)
+                self.edit_user_inputs['lastname'].set_placeholder_text(user_data.lastname)
+                self.edit_user_inputs['national'].set_placeholder_text(user_data.national_number)
+                self.edit_user_inputs['phone'].set_placeholder_text(user_data.phone)
+                self.edit_user_inputs['username'].set_placeholder_text(user_data.user_name)
+                # self.edit_user_inputs['password'].set_placeholder_text(user_data.password)
+
+        
+        def update_user_action(self, username, name, lastname, phone, national, new_username, password, show_error_callback, show_success_callback):
+            try:
+                update_user_by_username(session, username, name, lastname, phone, national, new_username, password)
+                show_success_callback(f'The user information has been changed.')
+                self.employee_edit(self)
+                for v in list(self.edit_user_inputs.values()):
+                    v.textvariable.set('')
+
+            except Exception as e:
+                show_error_callback(e)
+                
+        
+        #------------------------------------------------------------
         def show_error_message(self, message:str | Exception=None):
             if message:
                 self.clear_success_message()
                 message = str(message)
-                print(message)
                 self.error_message_label.place(relx=.03, rely=.01)
                 self.error_message_label.configure(text=message)
                 Concur(lambda : self._clear_login_error(5)).start()
