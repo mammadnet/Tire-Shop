0888e5556f7f8b27e06b1d9fec7c35f1c729e08d
Parsa <parsa.fadaei7@gmail.com>
2025-06-06 15:09:25 +0330

    feat: add customer report panel with order summary

--- interface/panels.py
+++ interface/panels.py
@@ -1,5 +1,5 @@
 from customtkinter import *
-from .widgets import Item_button, Input, Btn, DropDown, render_text
+from .widgets import Item_button, Input, Btn, DropDown, render_text, create_updatable_labels
 from database import session, get_all_employees_json, create_new_user, create_product
 from database import remove_user_by_username, update_user_by_username, user_by_username, get_all_username
 from database import get_all_products_json, delete_product_by_name_and_size, get_product_by_id, get_product_by_id_json, update_product_by_id, get_all_employee_usernames
@@ -1617,9 +1617,14 @@ class EmployeeReportPanel(Panel):
         customer_report_btn.grid(row=1,column=0 , sticky="e")
         
         self.sell_report_table = None
+        self.customer_report_frame = None
         # Create table 
-        self.initialize_report_table(self)
-        self.insert_content_to_report_table(self.sell_report_table, get_all_orders(session))
+        # self.initialize_report_table(self)
+        # self.insert_content_to_report_table(self.sell_report_table, get_all_orders(session))
+        self.customer_report_dropdown = None
+        self.customer_report_table = None
+        self.customer_report_labels = {}
+        self.customer_report(self)
         # Show table by default
         self.current_view = None
         
@@ -1684,5 +1689,45 @@ class EmployeeReportPanel(Panel):
             for product in order.products:
                 vals = (order.id, product.brand, f"{product.width}/{product.ratio}/{product.rim}", product.price, order.customer.name, order.date)
                 table.insert(parent="", index=0, values=vals)
-    
+#---------------------------------------------------------------
+
+    def customer_report(self, window):
+        if self.customer_report_frame:
+            content_frame = self.customer_report_frame
+        else:
+            content_frame = CTkFrame(window, fg_color="#5B5D76")
+            self.customer_report_frame = content_frame
+            
+        content_frame.place(relheight=.9, relwidth=.8, relx=.02, rely=.05)
+        content_frame.rowconfigure(tuple(range(0, 9)), weight=10)
+        content_frame.columnconfigure(tuple(range(1,4)), weight=10, pad=20, uniform='a')
+        content_frame.columnconfigure(0, weight=1, uniform='a')
+        content_frame.columnconfigure(4, weight=1, uniform='a')
+        
+        dropdown_frame = CTkFrame(content_frame, fg_color="red")
+        dropdown_frame.place(relx=0.5, rely=0.05, relwidth=1, relheight=0.15,anchor="n")
+        dropdown_frame.rowconfigure((0, 1), weight=1)
+        dropdown_frame.columnconfigure((0,1,2,3), weight=1)
+        
+        table_frame = CTkFrame(content_frame, fg_color="blue")
+        table_frame.place(relx=0, rely=0.2, relwidth=1, relheight=0.8, anchor='nw')
+        
+        # Add dropdown for customer selection
+        customers = get_all_customers(session)
+        combo_items = [f'{customer.id}:{customer.name}' for customer in customers]
+
+        if self.customer_report_dropdown:
+            self.customer_report_dropdown.place_forget()
+            self.customer_report_dropdown.destroy()
+        else:
+            self.customer_report_dropdown = DropDown(dropdown_frame, width=250, variable=StringVar(value=render_text("انتخاب مشتری:")), command=lambda x: self.customer_report_action(x))
+            self.customer_report_dropdown.configure(values=combo_items)
+
+        self.customer_report_dropdown.grid(row=0, column=0)
+        
+        self.initialize_customer_report_table(table_frame)
+        self.customer_report_label_total_buy = create_updatable_labels(dropdown_frame, render_text("مجموع خرید:"), 0, 2, "total_buy", container=self.customer_report_labels)
+        self.customer_report_label_total_orders = create_updatable_labels(dropdown_frame, render_text("تعداد سفارشات:"), 1, 2, "total_orders", container=self.customer_report_labels)
+        
+        
     
