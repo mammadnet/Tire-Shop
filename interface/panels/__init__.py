4b025f6ffb730bf6893f060cbb7f58ec64be45aa
Mohsen <moohamadiann@gmail.com>
2025-06-08 04:31:02 +0330

    Add panels module initialization file

---
+++ interface/panels/__init__.py
@@ -0,0 +1,11 @@
+from .manager.dashboard import ManagerDashboardPanel
+from .manager.product import ManagerProductPanel
+from .manager.employee import ManagerEmployeePanel
+from .manager.report import ManagerReportPanel
+
+from .admin.employee import AdminEmployeePanel
+from .admin.backup import AdminBackupPanel
+from .admin.restore import AdminRestorePanel
+
+from .employee.report import EmployeeReportPanel
+from .employee.sell import EmployeeSellPanel
